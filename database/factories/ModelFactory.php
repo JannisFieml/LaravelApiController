e3efa82aaa23540345f6cd39fab842f074007ab9
Jannis Fieml <jannis@fieml.com>
2021-05-15 00:00:41 +0200

    initial commit

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace VendorName\Skeleton\Database\Factories;
+namespace Jannisfieml\LaravelApiGenerator\Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 
--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace VendorName\Skeleton\Tests;
+namespace Jannisfieml\LaravelApiGenerator\Tests;
 
 class ExampleTest extends TestCase
 {
